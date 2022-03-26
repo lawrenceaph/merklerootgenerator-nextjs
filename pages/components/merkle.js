21c19b1f373f45451fe3238aacdb7ca4d3a5f98f
lawrenceaph <76676277+lawrenceaph@users.noreply.github.com>
2022-03-26 15:52:36 +0800

    added comments

--- pages/components/merkle.js
+++ pages/components/merkle.js
@@ -1,18 +1,35 @@
 
 
 import { useForm } from "react-hook-form";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { MerkleTree } from "merkletreejs";
 import keccak256 from "keccak256";
-import toast from "react-hot-toast";
 import Link from "next/link";
 import Head from "next/head";
 
+//React-hot-toast is utilized to provide some feedback that a hash or proof has been generated
+import toast from "react-hot-toast";
+
+
+
+
+
 export default function Merkle(){
 
+// Variables to handle state:
+
+//Root Hash stores the Merkle Root hash, once generated
 const [theRootHash, setTheRootHash] = useState();
-const [theMerkleOne, setTheMerkleOne] =useState();
-const [sampleProof, setSampleProof]=useState();
+
+// theMerkleTree is the actual Merkle Tree
+const [theMerkleTree, setTheMerkleTree] =useState();
+
+// sampleProof stores the generated Merkle Proof
+const [theProof, setTheProof]=useState();
+
+
+// The function below takes data, separated by commas, and generates a Merkle Tree using the data as leaf nodes
+// Once the Merkled Tree is generated, it computes the root hash and stores it in the state variable "theRootHash" 
 
 const generateMerkleHash = async(data)=> {
     const {addresses} = data;
@@ -26,38 +43,38 @@ const generateMerkleHash = async(data)=> {
     const merkleOne = new MerkleTree(leafNodes, keccak256, { sortPairs:true});
     
     const rootHash1 = "0x" + merkleOne.getRoot().toString('hex');
-    
-    
+        
     setTheRootHash(rootHash1);
-    setTheMerkleOne(merkleOne);
+    setTheMerkleTree(merkleOne);
     
 }
 
+// The function below takes in data and generates the necessary Merkle Proof, providing certainty that
+// the address or hash was in the data set used to creat the Merkle Tree. If there is no value produced, then the address
+// or hash sent was not part of the Merkle Tree. Smart contracts that possess a root hash of the Merkle Tree can be sent the Merkle Proof for
+// an address to determine if that address was in a white list. 
+
 const generateProof = async(data) => {
   const {address}= data
-  const merkleProof =await theMerkleOne.getHexProof((keccak256(address)))
-  const formattedProof = merkleProof.toString().replaceAll('"', '"');
-  const finalProof = formattedProof.split(',');
-  setSampleProof(merkleProof);
+  const merkleProof =await theMerkleTree.getHexProof((keccak256(address)))
+  
+  setTheProof(merkleProof);
 
    
 }
 
 
+const { register, handleSubmit,formState: { errors } } = useForm();
 
-
-const { register, handleSubmit, watch, formState: { errors } } = useForm();
- 
-console.log(watch("example")); // watch input value by passing the name of it
-const onSubmit = (data) => {
+//The function below is triggered by form submission. It takes in data and passes it to the function above (generateMerkleHash) which in turn generates the Merkle Tree and Merkle Root hash.
+const generateMerkleTree = (data) => {
  generateMerkleHash(data);toast.success("Root Hash Generated!")
 }
 
-const goSubmit = (data) => {generateProof(data);toast.success('Proof Generated!')}
+// The function below is triggered by form submission. It takes in data and passes it to the function above (generateProof) which in turn generates the Merkle Proof.
+const generateProofforAddress = (data) => {generateProof(data);toast.success('Proof Generated!')}
 
-console.log("The Merkle:",theMerkleOne);
-console.log(theRootHash)
-console.log(sampleProof)
+
 return(
 <>
 <Head>
@@ -84,7 +101,7 @@ return(
 {theRootHash!=null && <p onClick={() => {navigator.clipboard.writeText(theRootHash);toast.success(`Copied!`)}} className="text-sky-700 border border-1 border-sky-700 cursor-pointer rounded-lg p-2">Copy Hash</p>}
 </div>
 }
-    <form className="flex flex-col items-center justify-center m-2" onSubmit={handleSubmit(onSubmit)}>
+    <form className="flex flex-col items-center justify-center m-2" onSubmit={handleSubmit(generateMerkleTree)}>
       
       <textarea className="w-full m-2 border-2 p-2 text-sky-600 border-sky-700 text-lg rounded-lg" rows={10}
        defaultValue={'Replace this text with the wallet addresses (or other values for your use case)  separated by a comma. Spaces and quotes are removed automatically upon submission. '}
@@ -98,23 +115,26 @@ return(
   
 </div>
 <div className="">
-  {theRootHash && theMerkleOne &&
+  {theRootHash && theMerkleTree &&
 <>
 <div className="text-sky-700">
   Get the Merkle proof for an address here:
   </div>
 
-<form className="flex flex-col items-center justify-center m-2" onSubmit={handleSubmit(goSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      
+<form className="flex flex-col items-center justify-center m-2" onSubmit={handleSubmit(generateProofforAddress)}>
+      {/* register  input into the hook by invoking the "register" function */}
       {/* include validation with required or other standard HTML validation rules */}
+
       <input className="w-full m-2 border-2 p-2 text-sky-600 border-sky-700 text-lg rounded-lg" 
        defaultValue={'Address'}
         {...register("address", { required: true })} />
+      
       {/* errors will return when field validation fails  */}
+      
       {errors.exampleRequired && <span>This field is required</span>}
       
       <input className="border rounded-lg border-2 border-sky-700 bg-sky-600 p-2 text-white" type="submit" />
+    
     </form>
     </>
 }
@@ -123,14 +143,14 @@ return(
 </div>
 
 <div>
-  {sampleProof && 
+  {theProof && 
 
 <div className="break-all">
 <div className="font-semibold text-center">
 Your Merkle Proof:
 </div>
 
-[&ldquo;{sampleProof.join('","')}&ldquo;]
+[&ldquo;{theProof.join('","')}&ldquo;]
 
   </div>
   }
@@ -141,10 +161,16 @@ Your Merkle Proof:
 Found a bug? <br/> Let me know via the github repo: <Link  href="https://github.com/lawrenceaph/merklerootgenerator-nextjs"><a className="font-bold">Here 💖</a></Link>
 
 </div>
-<div className="text-center text-sky-700">
-  Building NFT smart contracts? Check out: <br/>
-  <Link href='https://github.com/hashlips-lab'><a className=" text-center font-semibold text-sky-900 cursor-pointer">HashLips Lab </a></Link></div>
 
+<div className="text-center text-sky-700">
+  Building NFT smart contracts? Check out: 
+
+  <div className="text-center text-sky-700 font-semibold underline">
+<Link href='https://unstoppable.gallery'>Unstoppable.Gallery</Link>
+</div>
+<div>
+  <Link href='https://github.com/hashlips-lab'><a className="underline text-center font-semibold text-sky-700 cursor-pointer">HashLips Lab </a></Link></div>
+  </div>
 </div>
 </div>
 </>
